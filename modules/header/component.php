3b815636a86bbe2901ca3ed4c1b03237a310c6eb
Sunny <sunny@sunnymorgan.com>
2021-10-16 13:30:29 -0400

    THEME_LOGO

--- modules/header/component.php
+++ modules/header/component.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Component: Header
  *
@@ -9,70 +10,70 @@
  */
 class X_Header extends X_Component {
 
-  public static function frontend($data) {
-    ?>
-    <header <?php parent::render_attributes($data['attr']); ?>>
+	public static function frontend( $data ) {
+		?>
+		<header <?php parent::render_attributes( $data['attr'] ); ?>>
 
-      <div class="site-header__inner">
+			<div class="site-header__inner">
 
-        <div class="site-header__branding">
-          <a href="<?php echo esc_url(home_url('/')); ?>" class="site-header__title" rel="home" itemprop="headline">
-            <img class="site-header__logo" width="300" height="100" src="<?php echo get_stylesheet_directory_uri(); ?>/dist/images/logo.svg" loading="lazy" alt="<?php echo esc_attr(get_bloginfo('name')); ?>" />
-          </a>
-        </div>
+				<div class="site-header__branding">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-header__title" rel="home" itemprop="headline">
+						<img class="site-header__logo" width="300" height="100" src="<?php echo THEME_LOGO; ?>" loading="lazy" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
+					</a>
+				</div>
 
-        <?php X_Menu_Toggle::render(); ?>
+				<?php X_Menu_Toggle::render(); ?>
 
-        <div class="site-header__menus">
-          <?php
-            X_Menu_Additional::render([
-              'attr' => [
-                'class' => ['site-header__additional', 'desktop-menu']
-              ]
-            ]);
-          ?>
+				<div class="site-header__menus">
+					<?php
+					X_Menu_Additional::render( [
+						'attr' => [
+							'class' => [ 'site-header__additional', 'desktop-menu' ]
+						]
+					] );
+					?>
 
-          <?php
-            X_Menu_Primary::render([
-              'attr' => [
-                'class' => ['site-header__main', 'desktop-menu']
-              ]
-            ]);
-          ?>
+					<?php
+					X_Menu_Primary::render( [
+						'attr' => [
+							'class' => [ 'site-header__main', 'desktop-menu' ]
+						]
+					] );
+					?>
 
-        </div>
+				</div>
 
-      </div>
+			</div>
 
-    </header>
-    <?php
-  }
+		</header>
+		<?php
+	}
 
-  public static function backend($args = []) {
+	public static function backend( $args = [] ) {
 
-    $placeholders = [
+		$placeholders = [
 
-      // optional
-      'attr'           => [],
+			// optional
+			'attr' => [],
 
-    ];
-    $args = wp_parse_args($args, $placeholders);
+		];
+		$args         = wp_parse_args( $args, $placeholders );
 
-    // classes
-    if (!isset($args['attr']['class'])) {
-      $args['attr']['class'] = [];
-    }
-    $args['attr']['class'][] = 'site-header';
+		// classes
+		if ( ! isset( $args['attr']['class'] ) ) {
+			$args['attr']['class'] = [];
+		}
+		$args['attr']['class'][] = 'site-header';
 
-    // id
-    $args['attr']['id'] = 'masthead';
+		// id
+		$args['attr']['id'] = 'masthead';
 
-    // Schema.org
-    $args['attr']['itemscope'] = null;
-    $args['attr']['itemtype']  = 'https://schema.org/WPHeader';
+		// Schema.org
+		$args['attr']['itemscope'] = null;
+		$args['attr']['itemtype']  = 'https://schema.org/WPHeader';
 
-    return $args;
+		return $args;
 
-  }
+	}
 
 }
